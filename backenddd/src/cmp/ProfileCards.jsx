1a6a1870dc99b30d2bbc6842478460efe4e055d7
nati439 <natichere77@gmail.com>
2026-08-12 22:31:04 -0500

    finish ProfileCards.jsx

--- backenddd/src/cmp/ProfileCards.jsx
+++ backenddd/src/cmp/ProfileCards.jsx
@@ -1,12 +1,12 @@
-// import React from 'react'
+import React from 'react'
 
-// export default function ProfileCards({person}) {
-//   return (
-//     <>
-//         <h3>{person.username}</h3>
-//         <p>{person.college}</p>
-//         <p>{person.year}</p>
-//     </>
+export default function ProfileCards({person}) {
+  return (
+    <>
+        <h3>{person.username}</h3>
+        <p>{person.college}</p>
+        <p>{person.year}</p>
+    </>
     
-//   )
-// }
+  )
+}
